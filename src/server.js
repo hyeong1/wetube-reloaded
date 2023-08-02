5c0fbdeefbeb32cfae475f3a462642a5ee07ecfc
hyeong1 <mi75625@gmail.com>
2023-08-02 22:13:05 +0900

    middleware

--- src/server.js
+++ src/server.js
@@ -4,14 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const handelHome = (req, res) => {
-    return res.send("<h1>I still love you</h1>");
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
 };
-const handelLogin = (req, res) => {
-    return res.send({message: "Login Here"});
-}
-app.get("/", handelHome);
-app.get("/login", handelLogin);
+
+const handelHome = (req, res) => {
+    return res.send("<h1>I love middleware</h1>");
+};
+
+app.get("/", logger, handelHome);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
