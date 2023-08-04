63048d7ae1e8a502a905c16a33b8be1c2b631143
hyeong1 <mi75625@gmail.com>
2023-08-04 14:02:51 +0900

    middlewares

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
+const loggerMiddleware = morgan("dev");
 
 const handelHome = (req, res) => {
     return res.send("<h1>I love middleware</h1>");
 };
+const login = (req, res) => {
+    return res.send("<h1>login</h1>");
+}
 
-app.get("/", logger, handelHome);
+app.use(loggerMiddleware);
+app.get("/", handelHome);
+app.get("/login", login);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
